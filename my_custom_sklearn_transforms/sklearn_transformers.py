5a3a93b2574b54d201cf1ee292ba4c4f08f3ec52
Wladimir Neto <wladimirteixeira@yahoo.com.br>
2020-08-21 22:48:43 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+import math
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -16,17 +16,84 @@ class DropColumns(BaseEstimator, TransformerMixin):
         return data.drop(labels=self.columns, axis='columns')
     
 class RemoverNotasMaioresQue10(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
-        self.columns = columns
-
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        data = X.copy()
-        df_remove = data.loc[(data[column] > 10)] 
-        # Retornamos um novo dataframe sem as linhas com notas maiores que 10
-        return data.drop(df_remove.index)
+        df_data_2 = X.copy()
+        df_remove = df_data_2.loc[(df_data_2['NOTA_GO'] > 10)] 
+        df_data_2 = df_data_2.drop(df_remove.index)
+        print(df_data_2.shape)
+        df_remove = df_data_2.loc[(df_data_2['NOTA_DE'] > 10)] 
+        df_data_2 = df_data_2.drop(df_remove.index)
+        print(df_data_2.shape)
+        df_remove = df_data_2.loc[(df_data_2['NOTA_MF'] > 10)] 
+        df_data_2 = df_data_2.drop(df_remove.index)
+        print(df_data_2.shape)
+        df_remove = df_data_2.loc[(df_data_2['NOTA_EM'] > 10)] 
+        df_data_2 = df_data_2.drop(df_remove.index)
+        return df_data_2
+    
+class PreencherDadosNulosOuEstranhos(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        df_data_2 = X.copy()
+        # Setar Lingua Inglesa Nula como 0 - Considero que ele nao fala outra lingua
+        df_data_3 = df_data_2
+        df_data_3.update(df_data_3['INGLES'].fillna(0))
+
+        # Setar Reprovações nulas como 0 - Considero que nunca foi reprovado
+        df_data_3.update(df_data_3['REPROVACOES_DE'].fillna(0))
+        df_data_3.update(df_data_3['REPROVACOES_EM'].fillna(0))
+        df_data_3.update(df_data_3['REPROVACOES_MF'].fillna(0))
+        df_data_3.update(df_data_3['REPROVACOES_GO'].fillna(0))
+
+
+
+        # Setar NOTA_GO de acordo com media geral para as notas zeradas
+        Filtro  = df_data_2['NOTA_GO'] > 0
+        DS = df_data_2[Filtro]
+        Media = DS['NOTA_GO'].mean()
+        df_data_3.update(df_data_3['NOTA_GO'].fillna(Media))
+
+        # Setar NOTA_DE de acordo com media geral  para as notas zeradas
+        Filtro  = df_data_2['NOTA_DE'] > 0
+        DS = df_data_2[Filtro]
+        Media = DS['NOTA_DE'].mean()
+        df_data_3.update(df_data_3['NOTA_DE'].fillna(Media))
+
+        # Setar NOTA_EM de acordo com media geral  para as notas zeradas
+        Filtro  = df_data_2['NOTA_EM'] > 0
+        DS = df_data_2[Filtro]
+        Media = DS['NOTA_EM'].mean()
+        df_data_3.update(df_data_3['NOTA_EM'].fillna(Media))
+
+        # Setar NOTA_MF de acordo com media geral  para as notas zeradas
+        Filtro  = df_data_2['NOTA_MF'] > 0
+        DS = df_data_2[Filtro]
+        Media = DS['NOTA_MF'].mean()
+        df_data_3.update(df_data_3['NOTA_MF'].fillna(Media))
+
+        # Setar H_AULA_PRES de acordo com media 
+        Filtro  = df_data_2['H_AULA_PRES'] > 0
+        DS = df_data_2[Filtro]
+        Media = DS['H_AULA_PRES'].mean()
+        df_data_3.update(df_data_3['H_AULA_PRES'].fillna(Media))
+
+        # Setar TAREFAS_ONLINE de acordo com media geral
+        Filtro  = df_data_2['TAREFAS_ONLINE'] > 0
+        DS = df_data_2[Filtro]
+        Media = DS['TAREFAS_ONLINE'].mean()
+        df_data_3.update(df_data_3['TAREFAS_ONLINE'].fillna(Media))
+
+        # Setar FALTAS de acordo com media geral
+        Filtro  = df_data_2['FALTAS'] > 0
+        DS = df_data_2[Filtro]
+        Media = DS['FALTAS'].mean()
+        df_data_3.update(df_data_3['FALTAS'].fillna(Media))
+        return df_data_3
         
          

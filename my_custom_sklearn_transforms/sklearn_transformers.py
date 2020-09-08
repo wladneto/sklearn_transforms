497bbf36618d44ea5eb456a865b414b330897c2e
Wladimir Neto <wladimirteixeira@yahoo.com.br>
2020-09-07 22:10:45 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,4 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-import math
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -15,73 +14,70 @@ class DropColumns(BaseEstimator, TransformerMixin):
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
     
-class RemoverNotasMaioresQue10(BaseEstimator, TransformerMixin):
+class Ingles(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
     def fit(self, X, y=None):
         return self
     
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
-        df_data_2 = X.copy()
-        df_remove = df_data_2.loc[(df_data_2['NOTA_GO'] > 10)] 
-        df_data_2 = df_data_2.drop(df_remove.index)
-        print(df_data_2.shape)
-        df_remove = df_data_2.loc[(df_data_2['NOTA_DE'] > 10)] 
-        df_data_2 = df_data_2.drop(df_remove.index)
-        print(df_data_2.shape)
-        df_remove = df_data_2.loc[(df_data_2['NOTA_MF'] > 10)] 
-        df_data_2 = df_data_2.drop(df_remove.index)
-        print(df_data_2.shape)
-        df_remove = df_data_2.loc[(df_data_2['NOTA_EM'] > 10)] 
-        df_data_2 = df_data_2.drop(df_remove.index)
-        return df_data_2
-    
-class PreencherDados(BaseEstimator, TransformerMixin):
+        data = X.copy()
+        data.update(data['INGLES'].fillna(0))
+        data['INGLES'] = data['INGLES'].astype('bool')
+        return data
+
+class NotasNaoPreenchidas(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
     def fit(self, X, y=None):
         return self
-    
+
     def transform(self, X):
-        df_data_2 = X.copy()
-        # Setar Lingua Inglesa Nula como 0 - Considero que ele nao fala outra lingua
-        df_data_3 = df_data_2
-        df_data_3.update(df_data_3['INGLES'].fillna(0))
-        # Setar Reprovações nulas como 0 - Considero que nunca foi reprovado
-        df_data_3.update(df_data_3['REPROVACOES_DE'].fillna(0))
-        df_data_3.update(df_data_3['REPROVACOES_EM'].fillna(0))
-        df_data_3.update(df_data_3['REPROVACOES_MF'].fillna(0))
-        df_data_3.update(df_data_3['REPROVACOES_GO'].fillna(0))
-        # Setar NOTA_GO de acordo com media geral para as notas zeradas
-        Filtro  = df_data_2['NOTA_GO'] > 0
-        DS = df_data_2[Filtro]
-        Media = DS['NOTA_GO'].mean()
-        df_data_3.update(df_data_3['NOTA_GO'].fillna(Media))
-        # Setar NOTA_DE de acordo com media geral  para as notas zeradas
-        Filtro  = df_data_2['NOTA_DE'] > 0
-        DS = df_data_2[Filtro]
-        Media = DS['NOTA_DE'].mean()
-        df_data_3.update(df_data_3['NOTA_DE'].fillna(Media))
-        # Setar NOTA_EM de acordo com media geral  para as notas zeradas
-        Filtro  = df_data_2['NOTA_EM'] > 0
-        DS = df_data_2[Filtro]
-        Media = DS['NOTA_EM'].mean()
-        df_data_3.update(df_data_3['NOTA_EM'].fillna(Media))
-        # Setar NOTA_MF de acordo com media geral  para as notas zeradas
-        Filtro  = df_data_2['NOTA_MF'] > 0
-        DS = df_data_2[Filtro]
-        Media = DS['NOTA_MF'].mean()
-        df_data_3.update(df_data_3['NOTA_MF'].fillna(Media))
-        # Setar H_AULA_PRES de acordo com media 
-        Filtro  = df_data_2['H_AULA_PRES'] > 0
-        DS = df_data_2[Filtro]
-        Media = DS['H_AULA_PRES'].mean()
-        df_data_3.update(df_data_3['H_AULA_PRES'].fillna(Media))
-        # Setar TAREFAS_ONLINE de acordo com media geral
-        Filtro  = df_data_2['TAREFAS_ONLINE'] > 0
-        DS = df_data_2[Filtro]
-        Media = DS['TAREFAS_ONLINE'].mean()
-        df_data_3.update(df_data_3['TAREFAS_ONLINE'].fillna(Media))
-        # Setar FALTAS de acordo com media geral
-        Filtro  = df_data_2['FALTAS'] > 0
-        DS = df_data_2[Filtro]
-        Media = DS['FALTAS'].mean()
-        df_data_3.update(df_data_3['FALTAS'].fillna(Media))       
-        return df_data_3 
+        #adiciona coluna de medias relacionadas
+        df = X.copy()
+        m_DE = df.loc[df["NOTA_DE"] >= 0, "NOTA_DE"].mean()
+        m_EM = df.loc[df["NOTA_EM"] >= 0, "NOTA_EM"].mean()
+        m_MF = df.loc[df["NOTA_MF"] >= 0, "NOTA_MF"].mean()
+        m_GO = df.loc[df["NOTA_GO"] >= 0, "NOTA_GO"].mean()
+        
+        df.update(df['NOTA_DE'].fillna(m_DE))
+        df.update(df['NOTA_EM'].fillna(m_EM))
+        df.update(df['NOTA_MF'].fillna(m_MF))
+        df.update(df['NOTA_GO'].fillna(m_GO))
+        
+        df.loc[df.NOTA_GO > 10, 'NOTA_DE'] = 10
+        df.loc[df.NOTA_EM > 10, 'NOTA_EM'] = 10 
+        df.loc[df.NOTA_MF > 10, 'NOTA_MF'] = 10 
+        df.loc[df.NOTA_GO > 10, 'NOTA_GO'] = 10 
+        
+        df.loc[df.NOTA_DE <= 0, 'NOTA_DE'] = m_DE
+        df.loc[df.NOTA_EM <= 0, 'NOTA_EM'] = m_EM 
+        df.loc[df.NOTA_MF <= 0, 'NOTA_MF'] = m_MF
+        df.loc[df.NOTA_GO <= 0, 'NOTA_GO'] = m_GO
+
+        return df  
+
+class MediasRelacionadas(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        #adiciona coluna de medias relacionadas
+        df = X.copy()
+        df['REPROVACOES_H'] = df['REPROVACOES_DE'] + df['REPROVACOES_EM']
+        df['REPROVACOES_E'] = df['REPROVACOES_MF'] + df['REPROVACOES_GO']
+        df['MEDIA_H'] = ((df['NOTA_DE']+df['NOTA_EM'])/2)/10
+        df['MEDIA_E'] = ((df['NOTA_MF']+df['NOTA_GO'])/2)/10
+        df['NOTA_DE'] = df['NOTA_DE']/10
+        df['NOTA_EM'] = df['NOTA_EM']/10
+        df['NOTA_MF'] = df['NOTA_MF']/10
+        df['NOTA_GO'] = df['NOTA_GO']/10
+        df['MEDIA_GERAL'] = (df['NOTA_DE']+df['NOTA_MF']+df['NOTA_EM']+df['NOTA_GO'])/4
+        
+        return df
